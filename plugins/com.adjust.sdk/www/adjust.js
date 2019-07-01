d7198a939aad9606eb81170b6391b51454270cdf
reggiestain <reggiestain@gmail.com>
2019-07-01 14:10:01 +0200

    First commit

---
+++ plugins/com.adjust.sdk/www/adjust.js
@@ -0,0 +1,197 @@
+function callCordova(action) {
+    var args = Array.prototype.slice.call(arguments, 1);
+
+    cordova.exec(
+        function callback(data) { },
+        function errorHandler(err) { },
+        'Adjust',
+        action,
+        args
+    );
+}
+
+function callCordovaStringify(action) {
+    var args = Array.prototype.slice.call(arguments, 1);
+
+    cordova.exec(
+        function callback(data) { },
+        function errorHandler(err) { },
+        'Adjust',
+        action,
+        [JSON.stringify(args)]
+    );
+}
+
+function callCordovaCallback(action, callback) {
+    var args = Array.prototype.slice.call(arguments, 2);
+
+    cordova.exec(callback,
+        function errorHandler(err) { },
+        'Adjust',
+        action,
+        args
+    );
+}
+
+var Adjust = {
+    create: function(adjustConfig) {
+        if (adjustConfig) {
+            adjustConfig.sdkPrefix = this.getSdkPrefix();
+        }
+
+        if (adjustConfig.hasAttributionListener()) {
+            callCordovaCallback('setAttributionCallback', adjustConfig.getAttributionCallback());
+        }
+
+        if (adjustConfig.hasEventTrackingSucceededListener()) {
+            callCordovaCallback('setEventTrackingSucceededCallback', adjustConfig.getEventTrackingSucceededCallback());
+        }
+
+        if (adjustConfig.hasEventTrackingFailedListener()) {
+            callCordovaCallback('setEventTrackingFailedCallback', adjustConfig.getEventTrackingFailedCallback());
+        }
+
+        if (adjustConfig.hasSessionTrackingSucceededListener()) {
+            callCordovaCallback('setSessionTrackingSucceededCallback', adjustConfig.getSessionTrackingSucceededCallback());
+        }
+
+        if (adjustConfig.hasSessionTrackingFailedListener()) {
+            callCordovaCallback('setSessionTrackingFailedCallback', adjustConfig.getSessionTrackingFailedCallback());
+        }
+
+        if (adjustConfig.hasDeferredDeeplinkCallbackListener()) {
+            callCordovaCallback('setDeferredDeeplinkCallback', adjustConfig.getDeferredDeeplinkCallback());
+        }
+
+        callCordovaStringify('create', adjustConfig);
+    },
+
+    trackEvent: function(adjustEvent) {
+        callCordovaStringify('trackEvent', adjustEvent);
+    },
+
+    setOfflineMode: function(enabled) {
+        callCordova('setOfflineMode', enabled);
+    },
+
+    appWillOpenUrl: function(url) {
+        callCordova('appWillOpenUrl', url);
+    },
+
+    setEnabled: function(enabled) {
+        callCordova('setEnabled', enabled);
+    },
+
+    setPushToken: function(pushToken) {
+        callCordova('setPushToken', pushToken);
+    },
+
+    setReferrer: function(referrer) {
+        callCordova('setReferrer', referrer);
+    },
+
+    isEnabled: function(callback) {
+        callCordovaCallback('isEnabled', callback);
+    },
+
+    gdprForgetMe: function() {
+        callCordova('gdprForgetMe');
+    },
+
+    getGoogleAdId: function(callback) {
+        callCordovaCallback('getGoogleAdId', callback);
+    },
+
+    getAmazonAdId: function(callback) {
+        callCordovaCallback('getAmazonAdId', callback);
+    },
+
+    getIdfa: function(callback) {
+        callCordovaCallback('getIdfa', callback);
+    },
+
+    getAdid: function(callback) {
+        callCordovaCallback('getAdid', callback);
+    },
+
+    getAttribution: function(callback) {
+        callCordovaCallback('getAttribution', callback);
+    },
+
+    getSdkVersion: function(callback) {
+        var sdkPrefix = this.getSdkPrefix();
+        callCordovaCallback('getSdkVersion', function(sdkVersion) {
+            callback(sdkPrefix + "@" + sdkVersion);
+        });
+    },
+
+    getSdkPrefix: function () {
+        return 'cordova4.17.1';
+    },
+
+    addSessionCallbackParameter: function(key, value) {
+        callCordova('addSessionCallbackParameter', key, value);
+    },
+
+    removeSessionCallbackParameter: function(key) {
+        callCordova('removeSessionCallbackParameter', key);
+    },
+
+    resetSessionCallbackParameters: function() {
+        callCordova('resetSessionCallbackParameters');
+    },
+
+    addSessionPartnerParameter: function(key, value) {
+        callCordova('addSessionPartnerParameter', key, value);
+    },
+
+    removeSessionPartnerParameter: function(key) {
+        callCordova('removeSessionPartnerParameter', key);
+    },
+
+    resetSessionPartnerParameters: function() {
+        callCordova('resetSessionPartnerParameters');
+    },
+
+    sendFirstPackages: function() {
+        callCordova('sendFirstPackages');
+    },
+
+    setTestOptions: function(testOptions) {
+        callCordova('setTestOptions', testOptions);
+    },
+
+    teardown: function(testParam) {
+        if(testParam === null || testParam === undefined || testParam !== 'test') {
+           return;
+        }
+        callCordova('teardown');
+    },
+
+    onResume: function(testParam) {
+        if(testParam === null || testParam === undefined || testParam !== 'test') {
+           return;
+        }
+        callCordova('onResume');
+    },
+
+    onPause: function(testParam) {
+        if(testParam === null || testParam === undefined || testParam !== 'test') {
+           return;
+        }
+        callCordova('onPause');
+    }
+};
+
+function onPause() {
+    callCordova('onPause');
+}
+
+function onResume() {
+    callCordova('onResume');
+}
+
+document.addEventListener('resume', onResume, false);
+document.addEventListener('pause', onPause, false);
+
+module.exports = Adjust;
